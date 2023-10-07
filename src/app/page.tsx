b2eb9d4e6a944b30bc7cf4494086a5fbe05ab4f7
Lemo Trano <tran1219@umn.edu>
2023-10-07 16:21:24 -0500

    added social's

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -131,7 +131,7 @@ export default function Home() {
             Contact
           </Heading>
           <Divider my="15px"/>
-          <SimpleGrid minChildWidth="50px" spacing={5}>
+          <SimpleGrid minChildWidth="100px" spacing={5}>
             <Button colorScheme='black' variant='outline'>Github</Button>
             <Button colorScheme="linkedin" variant='outline'>Linkedin</Button>
             <Button colorScheme='messenger' variant='outline'>Instagram</Button>
